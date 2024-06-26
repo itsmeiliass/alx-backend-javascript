479b9473bf7cbaee57dff011c3d88ac471bee320
iliass bargache <ibargache6@gmail.com>
2024-06-26 20:37:26 +0100

    fix

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,3 @@
-// 0-promise.js
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-      // Simulating an API call with a resolved Promise
-      resolve("API response");
-  });
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
 }
-
-export default getResponseFromAPI;
